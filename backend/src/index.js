6eeb157b9a22f14e4f2b91b10313585611f4d226
Falconfeather4 <huangsimiao2014@gmail.com>
2024-03-06 21:16:19 -0800

    removed cached repo

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express'
+
+import models from './models'
+import routes from './routes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// provides authenticated user as well as model context to the rest of the application
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
+});
+
+
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+const ipAddress = '0.0.0.0';
+app.listen(process.env.PORT, ipAddress, () => {
+  console.log(`App listening on ${process.env.PORT}`);
+});
