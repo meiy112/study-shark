2d9267071564f4fa6d00c0f24e78bdf1df666d70
maggie <maggie.weng112@gmail.com>
2024-03-12 03:39:13 -0700

    added addModal.js

--- frontend/components/Home/Home.js
+++ frontend/components/Home/Home.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function Home() {
   return (
-    <Text>Home - Outside.js</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Home - Outside.js</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F8FAFF", // Set the background color here
+  },
+});
