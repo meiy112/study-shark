55c83b97cc41c0cf71b697fac9ddc92dfaae537b
ehuang07 <ehuang07@students.cs.ubc.ca>
2024-03-16 22:02:39 -0700

    hooked all data fetching stuff to the mock server

--- frontend/components/Topic/Topic.js
+++ frontend/components/Topic/Topic.js
@@ -1,38 +1,17 @@
-import { useState, useEffect } from "react";
-import { View, FlatList, StyleSheet, ScrollView, Modal, TouchableOpacity, Button} from "react-native";
-import { Text, Appbar, Divider, Chip, Icon, Menu, IconButton, Dialog, Portal} from "react-native-paper"
+import { useState, useEffect, createContext, useContext } from "react";
+import { View, FlatList, ScrollView, TouchableOpacity} from "react-native";
+import { Text, Appbar, Divider, Chip, Icon, IconButton, Dialog, Portal} from "react-native-paper"
 import { SelectList } from 'react-native-dropdown-select-list'
 import { LinearGradient } from "expo-linear-gradient";
 import * as SplashScreen from "expo-splash-screen";
 
 
-// -------- FAKE DATA - TO BE REPLACED WITH API CALLS ----------
-const fakeTags = [ // sorry the colors r ugly
-  {name: "waves", color: '#5F2EB3'},
-  {name: "fluid dynamics", color: '#519dFF'}, 
-  {name: "electromagnetism", color: '#0096F3'}, 
-  {name: "kinematics", color: '#009F89'},
-  {name: "Energy", color: '#A84A5B'}];
-const fakeStudyMaterial = [
-  {title: "Wave Interference", type: "Notes", lastOpened: new Date()},
-  {title: "Simple Harmonic Motion", type: "Flashcards", lastOpened: new Date()},
-  {title: "Standing Waves", type: "Quiz", lastOpened: new Date()},
-  {title: "1", type: "Notes", lastOpened: new Date()},
-  {title: "2", type: "Flashcards", lastOpened: new Date()},
-  {title: "3", type: "Notes", lastOpened: new Date()},
-  {title: "4", type: "Quiz", lastOpened: new Date()}
-];
-const fakeMostUsedTag = { name: "Physics", color: "#5F2EB3" };
-//--------------------------------------------------------
-
-
+const ColorContext = createContext();
 export default function Topic({ route, navigation }) {
   const [topic, setTopic] = useState({topicId: "", title: "", description: ""});
   const [studyMaterial, setStudyMaterial] = useState([]);
   const [tags, setTags] = useState([]);
-  const [mostUsedTag, setMostUsedTags] = useState({name: "", color: "#FFFFFFFF"});
   const [isEditing, setIsEditing] = useState(false);
-
 
   // LOAD DATA------------------------------------
   // Splashscreen
@@ -42,10 +21,8 @@ export default function Topic({ route, navigation }) {
   load();
 
   // useEffects are sperate so they can run in parallel - api calls r rly slow
+  // fetch topic data
    useEffect(() => {
-    setMostUsedTags(fakeMostUsedTag); // TODO: get rid of this in the future
-
-    // fetch topic data
     async function fetchData() {
       try {
         const response = await fetch("/topic/" + route.params.id + "/general-info");
@@ -76,22 +53,38 @@ export default function Topic({ route, navigation }) {
    useEffect(() => {
     async function fetchData() {
       try {
-        // TODO: api call
-        setStudyMaterial(fakeStudyMaterial); 
+        const response = await fetch ("/topic/" + route.params.id + "/studymaterial/?type=None&sort=alphabetical")
+        const studyMaterial = await response.json();
+        setStudyMaterial(studyMaterial); 
       } catch (e) {
         console.log(e);
       }
     }
     fetchData();
    }, []);
-   
+
+  // fetch color data
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch ("/topic/" + route.params.id + "/studymaterial/?type=None&sort=alphabetical")
+        const studyMaterial = await response.json();
+        setStudyMaterial(studyMaterial); 
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+    }, []);
+
 
    if (!topic.id || studyMaterial.length === 0 || tags.length === 0) {
-    // prevent page from rendering before fonts are loaded
+    // prevent page from rendering before data is loaded
     return undefined;
   } else {
     SplashScreen.hideAsync();
   }
+    // ----------------------------------------------
 
    function handleSort() {
     // TODO
@@ -113,22 +106,24 @@ export default function Topic({ route, navigation }) {
    }
 
   return (
-    <View>
-      <Header topic={topic} color={mostUsedTag.color} navigation={navigation} />
-      <Divider />
-      <ScrollView style={{backgroundColor: '#F8FAFF'}} stickyHeaderIndices={[1]}>
-        <Info topic={topic} tags={tags} mostUsedTag={mostUsedTag} />
-        <SortAndEdit handleSort={handleSort} handleFilter={handleFilter} toggleEdit={toggleEdit}/>
-        <StudyMaterial studyMaterial={studyMaterial} topicId={topic.id} mostUsedTag={mostUsedTag} handleDelete={handleDelete} isEditing={isEditing} />
-      </ScrollView>
-    </View>
+    <ColorContext.Provider value={{color: route.params.color}}>
+      <View>
+        <Header topic={topic} color={route.params.color} navigation={navigation} />
+        <Divider />
+        <ScrollView style={{backgroundColor: '#F8FAFF'}} stickyHeaderIndices={[1]}>
+          <Info topic={topic} tags={tags} />
+          <SortAndEdit handleSort={handleSort} handleFilter={handleFilter} toggleEdit={toggleEdit}/>
+          <StudyMaterial studyMaterial={studyMaterial} topicId={topic.id} handleDelete={handleDelete} isEditing={isEditing} />
+        </ScrollView>
+      </View>
+    </ColorContext.Provider>
   );
 }
 
 function Header({ topic, color, navigation }) {
   return(
     <View>
-      <Appbar.Header style={{backgroundColor: color}}>
+      <Appbar.Header style={{backgroundColor: color.primary}}>
         <Appbar.BackAction color="#FFFFFF" onPress={() => navigation.goBack()} />
         <Appbar.Content title={topic.title} color="#FFFFFF" titleStyle={{fontWeight: '600', fontSize: 20, fontFamily: 'mon-sb'}}/>
         <Appbar.Action icon="cog-outline" color="#FFFFFF" onPress={() => navigation.navigate("Settings")}></Appbar.Action>
@@ -138,10 +133,11 @@ function Header({ topic, color, navigation }) {
   );
 }
 
-function Info({ topic, tags, mostUsedTag }) {
+function Info({ topic, tags }) {
+  const { color } = useContext(ColorContext);
   return (
     <LinearGradient
-    colors={[mostUsedTag.color, '#2B005A']} // TODO: HARDCODED VALUE
+    colors={[color.primary, color.gradient]} // TODO: HARDCODED VALUE
     start={{ x: 0.5, y: 0 }}
     end={{ x: 0.5, y: 1 }}
     style={{borderBottomRightRadius: 20, borderBottomLeftRadius: 20}}
@@ -149,20 +145,14 @@ function Info({ topic, tags, mostUsedTag }) {
       <View style={{padding: 15, borderBottomEndRadius: 20, borderBottomLeftRadius: 20}}>
         <Text style={{color: 'white', fontFamily: 'mon-sb'}} variant="titleSmall">Description</Text>
         <Text style={{padding:10, marginBottom:15, color: 'white', fontFamily: 'mon-m'}} variant="bodySmall">{topic.description}</Text>
-        <Tags tags={tags} mostUsedTag={mostUsedTag}/>
+        <Tags tags={tags}/>
       </View>
   </LinearGradient>
 
   )
 }
 
-function Tags({ tags, mostUsedTag }) {
-  let sortedTags = [mostUsedTag];
-  for (const tag of tags) {
-    if (tag.name !== mostUsedTag.name) {
-      sortedTags.push(tag);
-    }
-  }
+function Tags({ tags }) {
 
   const renderTag = (tag) => {
       return (
@@ -170,10 +160,8 @@ function Tags({ tags, mostUsedTag }) {
           <Chip
             key={tag.name}
             style={{
-              backgroundColor: tag.name === mostUsedTag.name? 'transparent' : tag.color,
-              borderColor: tag.name === mostUsedTag.name? 'white' : tag.color,
-              borderWidth: 2,
-              marginRight: 8,
+              backgroundColor: tag.color,
+              marginRight: 10,
             }}
             textStyle={{color: "#FFFFFF", fontFamily: 'mon-m'}}
           >
@@ -187,7 +175,7 @@ function Tags({ tags, mostUsedTag }) {
     <View>
       <FlatList
         horizontal
-        data={sortedTags}
+        data={tags}
         keyExtractor={(tag) => tag.name}
         renderItem={({ item }) => renderTag(item)}
         showsHorizontalScrollIndicator={false}
@@ -228,19 +216,19 @@ function SortAndEdit({ handleSort, handleFilter, toggleEdit }) {
   );
 }
 
-function StudyMaterial({ studyMaterial, topicId, mostUsedTag, handleDelete, isEditing }) {
+function StudyMaterial({ studyMaterial, topicId, handleDelete, isEditing }) {
   const studyMaterialComponents = studyMaterial.map((item) => {
-    return <StudyMaterialCard studyMaterial={item} topicId={topicId} mostUsedTag={mostUsedTag} handleDelete={handleDelete} isEditing={isEditing} key={item.title} />
+    return <StudyMaterialCard studyMaterial={item} topicId={topicId} handleDelete={handleDelete} isEditing={isEditing} key={item.title} />
   });
 
     return (
-      <View style={{flexDirection: 'row', flexWrap: "wrap", marginBottom: 180, padding: 8}}> 
+      <View style={{flexDirection: 'row', flexWrap: "wrap", marginBottom: 90, padding: 8}}> 
         {studyMaterialComponents}
       </View>
     )
 }
 
-function StudyMaterialCard({ studyMaterial, topicId, mostUsedTag, handleDelete, isEditing }) {
+function StudyMaterialCard({ studyMaterial, topicId, handleDelete, isEditing }) {
   const [numItems, setNumItems] = useState(0);
 
   const iconMap = {
@@ -264,32 +252,34 @@ function StudyMaterialCard({ studyMaterial, topicId, mostUsedTag, handleDelete, 
   return (
     <View style={{width: "45%", padding:15, paddingTop: 0, margin: 8, backgroundColor: '#FFFFFF', borderRadius: 20, shadowColor: 'black', shadowOpacity: 0.1, shadowRadius: 5}}>
       <TouchableOpacity onPress={onPress} disabled={isEditing}>
-        <CardHeader studyMaterial={studyMaterial} mostUsedTag={mostUsedTag} handleDelete={handleDelete} isEditing={isEditing}/>
+        <CardHeader studyMaterial={studyMaterial} handleDelete={handleDelete} isEditing={isEditing}/>
         <View style={{alignItems: 'center'}}>
           <Icon source={iconMap[studyMaterial.type]} size={60}/>
         </View>
         <View style={{marginTop: 15, marginBottom: 5}}>
           <Text style={{fontSize: 14, fontFamily: 'mon-m'}}>{studyMaterialTitle}</Text>
           <View style={{marginTop: 6}}>
-            <Text style={{fontSize: 10, color: '#414141', fontFamily: 'mon-l'}}>{studyMaterial.lastOpened.toDateString()}</Text>
+            <Text style={{fontSize: 10, color: '#414141', fontFamily: 'mon-l'}}>{studyMaterial.lastOpened}</Text>
           </View>
         </View>
-        <CardFooter studyNoteType={studyMaterial.type} numItems={numItems} mostUsedTag={mostUsedTag}/>
+        <CardFooter studyNoteType={studyMaterial.type} numItems={numItems} />
       </TouchableOpacity>
     </View>
   );
 }
 
-function CardHeader({ studyMaterial, mostUsedTag, handleDelete, isEditing }) {
+function CardHeader({ studyMaterial, handleDelete, isEditing }) {
   const [isVisible, setIsVisible] = useState(false);
   
   const showDialog = () => setIsVisible(true);
   const hideDialog = () => setIsVisible(false);
 
+  const { color } = useContext(ColorContext);
+
   return (
     <View style={{display:"flex", flexDirection: "row", alignItems: 'center', justifyContent: 'space-between'}}>
       <View style={{marginTop: 13, marginBottom: 13}}>
-        <View style={{backgroundColor: mostUsedTag.color, paddingVertical: 4, paddingHorizontal: 8, borderRadius: 10}}>
+        <View style={{backgroundColor: color.primary, paddingVertical: 4, paddingHorizontal: 8, borderRadius: 10}}>
           <Text style={{color: '#FFFFFF', fontSize: 10, fontFamily: 'mon-m'}}>{studyMaterial.type.toUpperCase()}</Text>
         </View>
       </View>
@@ -313,8 +303,9 @@ function CardHeader({ studyMaterial, mostUsedTag, handleDelete, isEditing }) {
   )
 }
 
-function CardFooter({ studyNoteType, numItems, mostUsedTag }) {
-  let footerText = {
+function CardFooter({ studyNoteType, numItems }) {
+  const { color } = useContext(ColorContext);
+  const footerText = {
     "Notes": "Pages",
     "Quiz": "Questions",
     "Flashcards": "Cards"
@@ -322,7 +313,7 @@ function CardFooter({ studyNoteType, numItems, mostUsedTag }) {
     
   return (
     <View style={{flexDirection: "row", marginTop: 5, alignItems: "center"}}>
-      <Text style={{height: 28, width: 28, padding: 5, borderColor: mostUsedTag.color, borderWidth: 1.5, borderRadius: 14, marginRight: 10, textAlign: "center", lineHeight: 15.5, fontFamily: 'mon-m'}}>{numItems}</Text>
+      <Text style={{height: 28, width: 28, padding: 5, borderColor: color.circle, borderWidth: 1.5, borderRadius: 14, marginRight: 10, textAlign: "center", lineHeight: 15.5, fontFamily: 'mon-m'}}>{numItems}</Text>
       <Text style={{fontFamily: 'mon-m', fontSize:13}}>{footerText[studyNoteType]}</Text>
     </View>
   )
