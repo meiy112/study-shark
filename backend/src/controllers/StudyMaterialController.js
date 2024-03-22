42c2aebd318da9accbf48b65a492772c53ce8b3a
oyik <oyik>
2024-03-22 12:54:28 -0700

    added good error messages

--- backend/src/controllers/StudyMaterialController.js
+++ backend/src/controllers/StudyMaterialController.js
@@ -4,27 +4,61 @@ const studyMaterialService = require('../services/StudyMaterialService');
 class StudyMaterialController {
   // gets all filtered and sorted study materials from a given topic 
   getFilteredSortedStudyMaterial(req, res) {
-    studyMaterialService.getFilteredSortedStudyMaterial(req.params.id, req.body.type, req.body.sort, (err, rows) => {
-      if (err) {
-        console.error('Error executing query:', err.message);
-        res.status(301).send('Error executing query: getFilteredSortedStudyMaterial, ', err.message);
-        return;
-      }
+    studyMaterialService.getFilteredSortedStudyMaterial(req.params.id, req.body.type, req.body.sort)
+    .then(rows => {
       res.send(rows);
+    })
+    .catch(err => {
+      // Return 'Bad Request' if topic ID is invalid
+      if (err.message == 'Error topic does not exist') {
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'Error topic does not exist: getFilteredSortedStudyMaterial'});
+      } else {
+        // return 'Internal Service Error' if anything strange happens in the query 
+        res.status(500).send({message: 'Internal Service Error', 
+                              details: 'Error executing query: getFilteredSortedStudyMaterial'});
+      }
+      return;
     });
   }
 
   
   // deletes given study material from topic
   deleteStudyMaterial(req, res) {
-    studyMaterialService.deleteStudyMaterial(req.params.id, req.params.title, req.username, (err, rows) => {
-      if (err) {
-        console.error('Error executing query:', err.message);
-        res.status(302).send('Error executing query: deleteStudyMaterial, ', err.message);
-        return;
-      }
+    studyMaterialService.deleteStudyMaterial(req.params.id, req.params.title, req.username)
+    .then(() => {
       res.send("Study material: " + req.params.title + " from Topic: " + req.params.id + " deleted successfully");
-    });
+    })
+    .catch(err => {
+      if (err.message == 'No username') {
+        if (req.expired == 'true') {
+          // return "Forbidden" if user was not authenticated because jwt expired
+          res.status(403).send({message: 'Forbidden', 
+                                  details: 'unidentified user tried to delete study material, jwt expired'});
+        } else {
+          // return "Forbidden" if user was not authenticated
+          res.status(403).send({message: 'Forbidden', 
+                                  details: 'unidentified user tried to delete study material'});
+        }
+      } else if (err.message == 'No topicId') {
+        // return "Bad Request" if no topicId is given
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'Error no topicId given: deleteStudyMaterial'});
+      } else if (err.message == 'User does not own topic') {
+        // return "Bad Request" if user does not own topic
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'Error user does not own topic: deleteStudyMaterial'});
+      } else if (err.message == 'Topic does not have study material') {
+        // return "Bad Request" if topic does not have study material
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'Error topic does not have study material: deleteStudyMaterial'});
+      } else {
+        // return 'Internal Service Error' if anything strange happens in the query 
+        res.status(500).send({message: 'Internal Service Error', 
+                              details: 'Error executing query: deleteStudyMaterial'});
+      }
+      return;
+    })
   }
 }
 
--- backend/src/services/StudyMaterialService.js
+++ backend/src/services/StudyMaterialService.js
@@ -5,20 +5,61 @@ class StudyMaterialService {
   // performs SQL query to get study materials.
 
   // gets all filtered and sorted study materials from a given topic 
-  getFilteredSortedStudyMaterial(topicId, type, sort, callback) {
-    if (sort == 'lastOpened') {
-      db.query('SELECT * FROM ContainsStudyMaterial WHERE topicId = ? AND type = ? ORDER BY lastOpened ASC', [topicId, type], (err, rows, fields) => {
-        callback(err, rows);
+  async getFilteredSortedStudyMaterial(topicId, type, sort) {
+
+    // function to check if topic does not exist
+    const checkTopicDoesNotExist = (topicId) => {
+      return new Promise((resolve, reject) => {
+          const exists = 'SELECT id FROM createstopic WHERE id = ?'; 
+          db.query(exists, [topicId], (err, rows, fields) => {
+              if (err) {
+                  reject(err);
+                  return;
+              }
+              resolve(rows.length == 0);
+          });
       });
-    } else {
-      db.query('SELECT * FROM ContainsStudyMaterial WHERE topicId = ? AND type = ? ORDER BY title ASC', [topicId, type], (err, rows, fields) => {
-        callback(err, rows);
-      });
+    };
+
+    try {
+      // if topic does not exist return error "Error topic does not exist"
+      if (await checkTopicDoesNotExist(topicId)) {
+        const err = new Error("Error topic does not exist");
+        throw err;
+      }
+      // If sort option is lastOpened, execute the query 
+      // we do this because the placeholder ? has issues with single quotes
+      if (sort == 'lastOpened') {
+        return new Promise ((resolve, reject) => {
+          db.query('SELECT * FROM ContainsStudyMaterial WHERE topicId = ? AND type = ? ORDER BY lastOpened ASC', [topicId, type], (err, rows, fields) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(rows);
+          });
+        })
+      } else {
+        // If sort option is alphebetical, execute the query with ORDER BY title ASC
+        // we do this because the placeholder ? has issues with single quotes
+        return new Promise ((resolve, reject) => {
+          db.query('SELECT * FROM ContainsStudyMaterial WHERE topicId = ? AND type = ? ORDER BY title ASC', [topicId, type], (err, rows, fields) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(rows);
+          });
+        })
+      }
+    } catch (error) {
+      throw error;
     }
   }
 
   // deletes given study material from topic
-  async deleteStudyMaterial(topicId, title, username, callback) {
+  async deleteStudyMaterial(topicId, title, username) {
+    // function to check if user does not have a topic
     const checkUserDoesNotHaveTopic = (username, topicId) => {
       return new Promise((resolve, reject) => {
           const exists = 'SELECT id FROM createstopic WHERE username = ? AND id = ?'; 
@@ -32,6 +73,7 @@ class StudyMaterialService {
       });
     };
 
+    // function to check if topic does not have a study material
     const checkTopicDoesNotHaveStudyMaterial = (title, topicId) => {
       return new Promise((resolve, reject) => {
           const exists = 'SELECT title FROM containsStudyMaterial WHERE topicId = ? AND title = ?'; 
@@ -46,27 +88,35 @@ class StudyMaterialService {
     };
 
     try {
-      if (!username) {
+      // if user is not authenticated return error "No username"
+      if (username == 'no user') {
         const err = new Error("No username");
-        callback(err,{});
+        throw err;
       } else if (!topicId) {
+        // if no topicId is given return error "No topicId"
         const err = new Error("No topicId");
-        callback(err,{});
+        throw err; 
       } else {
+        // if user does not own the topic return error "User does not own topic"
         if (await checkUserDoesNotHaveTopic(username, topicId)) {
           const err = new Error("User does not own topic");
-          callback(err,{});
+          throw err;
         }
-
+        // if topic does not have the study material return error "Topic does not have study material"
         if (await checkTopicDoesNotHaveStudyMaterial(title, topicId)) {
           const err = new Error("Topic does not have study material");
-          callback(err,{});
+          throw err;
         }
-
-        db.query('DELETE FROM containsstudymaterial WHERE topicId = ? AND title = ?', 
-          [topicId, title], (err, rows, fields) => {
-          callback(err, rows);
-        });
+        // delete study material from topic if there are no errors
+        return new Promise ((resolve, reject) => {
+          db.query('DELETE FROM containsstudymaterial WHERE topicId = ? AND title = ?', [topicId, title], (err, rows, fields) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(rows);
+          });
+        })
       }
     } catch (error) {
       throw error; 
