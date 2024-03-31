1b849d4286125b2967d8166dca8905bb0bf8883a
oyik <oyik>
2024-03-31 00:13:39 -0700

    made one more endpoint and bug fixes

--- backend/src/controllers/AdminController.js
+++ backend/src/controllers/AdminController.js
@@ -4,8 +4,7 @@ const adminService = require('../services/AdminService');
 class AdminController {
   // gets all user specified topics
   getAdminTopics(req, res) {
-    let newStr = req.query.query.slice(1, -1);
-    adminService.getAdminTopics(newStr, req.username)
+    adminService.getAdminTopics(req.query.query, req.username)
     .then (rows => {
       res.send(rows);
     })
@@ -24,6 +23,44 @@ class AdminController {
         // return 'Bad Request' if query is invalid
         res.status(400).send({message: 'Bad Request', 
                               details: 'Invalid query string: getAdminTopics'});
+      }
+      return;
+    });
+  }
+
+  // returns tuples given attribute list and table
+  postAdminTable(req, res) {
+    adminService.postAdminTable(req.query.name, req.body.attrList, req.username)
+    .then (rows => {
+      res.send(rows);
+    })
+    .catch (err => {
+      if (err.message == 'Not admin') {
+        if (req.expired == 'true') {
+          // return "Bad Request" if user is not admin because jwt expired
+          res.status(400).send({message: 'Bad Request', 
+                                  details: 'unidentified user tried to access admin info, jwt expired'});
+        } else {
+          // return "Bad Request" if user is not admin
+          res.status(400).send({message: 'Bad Request', 
+                                  details: 'unidentified user tried to access admin info'});
+        }
+      } else if (err.message == 'table does not exist') {
+        // return "Bad Request" if table does not exist
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'table does not exist in the database'});
+      } else if (err.message == 'no attributes provided') {
+        // return "Bad Request" if no attributes provided
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'no attributes provided'});
+      } else if (err.message == 'attribute not in table') {
+        // return "Bad Request" if one of the attributes in the list is not in the table
+        res.status(400).send({message: 'Bad Request', 
+                              details: 'one of the attributes is not in the given table'});
+      } else {
+        // return 'Internal Service Error' if query is invalid
+        res.status(500).send({message: 'Internal Service Error', 
+                              details: 'Error executing query: postAdminTable'});
       }
       return;
     });
