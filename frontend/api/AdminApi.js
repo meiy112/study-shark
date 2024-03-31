2a14b81d6b50b8adb10bebacc0838757f8b291d7
ehuang07 <ehuang07@students.cs.ubc.ca>
2024-03-30 17:47:14 -0700

    finished UI for Projection rubric query

--- frontend/api/AdminApi.js
+++ frontend/api/AdminApi.js
@@ -2,9 +2,18 @@ import { requests } from "./RequestTemplate";
 
 export const adminApi = {
   getTopics,
+  getTables,
 }
 
 async function getTopics(token, queryString) {
   const data = await requests.getRequest(token, `/admin/topic/?query=${queryString}`);
+  return data;
+}
+
+async function getTables(token, tableName, attrs) {
+  const data = await requests.postRequest(
+    token, 
+    `/admin/table/?name=${tableName}`,
+    {"attrList": attrs});
   return data;
 }
